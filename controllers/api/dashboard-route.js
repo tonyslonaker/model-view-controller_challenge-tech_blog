be40563747fcd5ae183c9a175bc9b1400297d36d
Tony Slonaker <tony.slonaker@gmail.com>
2021-10-30 16:29:47 -0600

    update dashboard route

---
+++ controllers/api/dashboard-route.js
@@ -0,0 +1,86 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Comment, Post, User } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'created_at',
+            'id',
+            'post_body',
+            'title'
+        ],
+        order: [['created_at', 'DESC']],        
+        include: [
+            {
+                model: Comment,
+                attributes: ['comment_text', 'created_at', 'id', 'post_id', 'user_id',],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/', (req, res) => {
+    res.render('dashboard', { loggedIn: true });
+})
+
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findByPk(req.params.id, {
+        attributes: [
+            'created_at',
+            'id',
+            'post_body',
+            'title'
+            
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['comment_text', 'created_at', 'id', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        if (dbPostData) {
+            const post = dbPostData.get({ plain: true });
+
+            res.render('edit-post', {
+                post,
+                loggedIn: true
+            });
+        } else {
+            res.status(404).end();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
