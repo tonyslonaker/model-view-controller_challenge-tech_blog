fb50ffb449662c3aef9e45dd865add4b54188c40
Tony Slonaker <tony.slonaker@gmail.com>
2021-10-31 18:50:39 -0600

    update code

--- utils/helper.js
+++ utils/helper.js
@@ -1,18 +1,4 @@
 module.exports = {
     format_date: date => {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
-    },
-    //format_plural: (word, amount) => {
-        //if (amount !== 1) {
-          //  return `${word}s`;
-        }
-        //return word;
-    //},
-    //format_time: (time) => {
-      //  return `${new Date(time).toLocaleTimeString('en-US', {
-        //    hour: 'numeric',
-          //  minute: 'numeric',
-            //timeZone: 'America/MST'
-        //})}`;
-    //}
-//}
+    };
